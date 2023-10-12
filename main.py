091b0753a37fbaa3c7966dc617da43abae1e0f66
Danish Adellio Alesaka <94503315+hayabusa1365@users.noreply.github.com>
2023-10-12 11:17:20 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+from flask import Flask
+
+import random
+
+ 
+
+app = Flask(__name__)
+
+ 
+
+facts_list = [
+
+    "Kebanyakan orang yang menderita kecanduan teknologi mengalami stres yang kuat ketika mereka berada di luar area jangkauan jaringan atau tidak dapat menggunakan perangkat mereka",
+
+    "Menurut sebuah studi yang dilakukan pada tahun 2018, lebih dari 50% orang berusia 18 hingga 34 tahun menganggap diri mereka bergantung pada ponsel pintar mereka.",
+    
+    "Menurut sebuah studi tahun 2019, lebih dari 60% orang merespons pesan pekerjaan di ponsel mereka dalam waktu 15 menit setelah pulang kerja.",
+
+    "Salah satu cara untuk memerangi ketergantungan teknologi adalah dengan mencari kegiatan yang membawa kesenangan dan meningkatkan suasana hati."
+
+]
+
+ 
+
+@app.route("/")
+
+def home():
+
+    return '<h1>Main Page</h1><a href="/random_fact">View a random fact!</a>'
+
+ 
+
+@app.route("/random_fact")
+
+def random_fact():
+
+    return f'<p>{random.choice(facts_list)}</p>'
+
+# Impor
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    # Variabel yang memungkinkan penghitungan konsumsi energi peralatan
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+# Halaman pertama
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Halaman kedua
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+# Halaman ketiga
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Perhitungan
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
+
+ 
+
+app.run(debug=True)
